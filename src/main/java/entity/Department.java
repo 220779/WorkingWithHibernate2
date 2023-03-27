9d687f8dd9f4197557e283a829d1295b27bda52d
220779 <102145019+220779@users.noreply.github.com>
2023-03-27 12:59:46 +0300

    [fix]  - Fixing Main class

--- src/main/java/entity/Department.java
+++ src/main/java/entity/Department.java
@@ -5,17 +5,17 @@ import jakarta.persistence.*;
 @Entity
 public class Department {
 
-    private int id;
+    private int department_id;
     private String name;
 
     @Id
     @Column(name = "id")
-    public int getId() {
-        return id;
+    public int getDepartment_id() {
+        return department_id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
     }
 
     @Basic
@@ -39,14 +39,14 @@ public class Department {
 
         Department that = (Department) o;
 
-        if (id != that.id) return false;
+        if (department_id != that.department_id) return false;
         if (name != null ? !name.equals(that.name) : that.name != null) return false;
         return true;
     }
 
     @Override
     public int hashCode() {
-        int result = id;
+        int result = department_id;
         result = 31 * result + (name != null ? name.hashCode() : 0);
         return result;
     }
